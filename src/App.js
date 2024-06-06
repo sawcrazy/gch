5088273f9fe98162bb8bf77f7b94b452bf35465c
Andrey <sawcrazy@yandex.ru>
2024-06-06 16:37:36 +0300

    add users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
-function App() {
+export const App = () => {
+    const [users , setUsers] = useState([]);
+
+    useEffect(()=>{
+            const fetchUsers = async () =>{
+                const usersNameJson = await fetch('https://jsonplaceholder.typicode.com/users');
+                const users= await usersNameJson.json();
+                setUsers(users);
+            };
+
+            fetchUsers();
+        },
+        []
+    );
+
+    const renderUsers = () =>{
+        const shotUsers = [...users];
+        return shotUsers.splice(0,5)
+            .map((item) =>{
+                return (
+                    <div key={item.id}>
+                        <ul>
+                            <li>Имя : {item.name}</li>
+                            <li>Никнейм : {item.username}</li>
+                            <li>email : {item.email}</li>
+                            <li>улица : {item.address.street}</li>
+                            <li>suite : {item.address.suite}</li>
+                            <li>город : {item.address.city}</li>
+                            <li>zipcode : {item.address.zipcode}</li>
+                            <li>lat : {item.address.geo.lat}</li>
+                            <li>lng : {item.address.geo.lng}</li>
+                            <li>телефон : {item.phone}</li>
+                            <li>website : {item.website}</li>
+                            <li>name : {item.company.name}</li>
+                            <li>catchPhrase : {item.company.catchPhrase}</li>
+                            <li>bs : {item.company.bs}</li>
+                        </ul>
+                    </div>
+                )
+
+
+        })
+    }
+    const next = () =>{
+        const nextUsers = [...users];
+        setUsers(nextUsers.splice(5,5));
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          {renderUsers()}
+          <button onClick={next}>
+             next
+          </button>
+      </div>
+  )
+
 }
 
-export default App;
